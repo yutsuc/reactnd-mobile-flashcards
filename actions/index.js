e73389d6de86aadf87901b9e2dc1c9f2973aceb8
Jessica <yutsuc@gmail.com>
2020-04-29 10:59:14 -0700

    feat: Add actions

---
+++ actions/index.js
@@ -0,0 +1,25 @@
+export const RECEIVE_DECKS = "RECEIVE_DECKS";
+export const ADD_DECK = "ADD_DECK";
+export const ADD_CARD = "ADD_CARD";
+
+export function receiveDecks(decks) {
+    return {
+        type: RECEIVE_DECKS,
+        decks,
+    };
+}
+
+export function addDeck(deckName) {
+    return {
+        type: ADD_DECK,
+        deckName,
+    };
+}
+
+export function addCard(deckName, card) {
+    return {
+        type: ADD_CARD,
+        deckName,
+        card,
+    };
+}
