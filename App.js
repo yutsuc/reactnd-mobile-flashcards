eb9b3b9fcff47e4f6a13aceceebad6a9140033da
Jessica <yutsuc@gmail.com>
2020-05-07 09:14:45 -0700

    fix: Take out arrow function on lifecycle event

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ const Home = () => {
 const Stack = createStackNavigator();
 
 class App extends React.Component {
-    componentDidMount = () => {
+    componentDidMount() {
         setLocalNotification();
     }
 
